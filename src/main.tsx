16c39a17aa82abfbbae2c57bf6e91ae3d07fa102
Moqleh <moh.oglah@gmail.com>
2026-09-10 22:13:18 +0300

    Load mobile clarity refinements

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './reference.css';
+import './mobile-clarity.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
