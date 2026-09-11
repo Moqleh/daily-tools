343b3346a6bdd75f1196c131c187f9c1cec99ad6
Moqleh <moh.oglah@gmail.com>
2026-09-11 06:02:47 +0300

    Activate real calculator interfaces

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './reference.css';
 import './mobile-clarity.css';
 import './native-dialogs';
 import './game-fixes';
+import './components/calculators/calculator-launcher';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
