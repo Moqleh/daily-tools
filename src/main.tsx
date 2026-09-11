2242c944ccd8b33ddd85d6062214073f221af427
Moqleh <moh.oglah@gmail.com>
2026-09-11 05:52:37 +0300

    Load branded dialog system

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import './reference.css';
 import './mobile-clarity.css';
+import './native-dialogs';
 import './game-fixes';
 
 createRoot(document.getElementById('root')!).render(
